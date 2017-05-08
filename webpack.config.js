f760cfadc1ca86c1ac576ca0900c568b1a000865
Sandeep Khandewale <learner1024@gmail.com>
2017-05-08 15:42:23 +0530

    eslint

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
-    filename: "[name].[contenthash].css"
+    filename: '[name].[contenthash].css',
 });
 
 const PATHS = {
@@ -39,8 +39,8 @@ const commonConfig = {
         rules: [
             {test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/,  include: PATHS.src},
             {test: /\.(pug|jade)$/, use: 'pug-loader', exclude: /node_modules/,  include: PATHS.src},
-            {test: /\.scss$/,use: extractSass.extract({use: ['css-loader','sass-loader'], fallback: 'style-loader'})}
-        ]
+            {test: /\.scss$/,use: extractSass.extract({use: ['css-loader','sass-loader'], fallback: 'style-loader'})},
+        ],
     },
     output: {
         path: PATHS.dist,
@@ -55,7 +55,7 @@ const devConfig = () => {
         devServer: {
             publicPath: '/dist',
             historyApiFallback: true,
-            overlay: true
+            overlay: true,
         },
         plugins : commonPlugins.concat(devPlugins),
         
