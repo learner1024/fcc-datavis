c066ab21c53ec8ed6a1f5eaeb55c072c6caa806d
Sandeep Khandewale <learner1024@gmail.com>
2017-05-08 11:27:42 +0530

    webpack- merge way

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,51 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
 
-module.exports = (env) => {
-    const wpConfig = {
-        context: path.resolve(__dirname, 'src'),
-        entry: {
-            apps: [ "./camper-leaderboard/app.js", "./md-previewer/app.js"],
-            fx: ['react', 'react-dom']
-        },
-        module: {
-            rules: [
-                {test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/,  include: path.resolve(__dirname, 'src'),}
-            ]
-        },
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            publicPath: env == "production" ? 'https://learner1024.github.io/fcc-datavis/dist' : "",
-            filename: "[name].bundle.js"
-        },
-        plugins: [
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'fx'
-            }),
-            new HtmlWebpackPlugin({
-                template: '!!pug-loader!src/landing/views/index.pug',
-                filename: path.join(__dirname, 'index.html')
-            })
-        ],
-        devServer: {
-            historyApiFallback: true
-        }
-    };
-    return wpConfig;
+const commonConfig = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        apps: [ "./camper-leaderboard/app.js", "./md-previewer/app.js"],
+        fx: ['react', 'react-dom']
+    },
+    module: {
+        rules: [
+            {test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/,  include: path.resolve(__dirname, 'src')}
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "[name].bundle.js"
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'fx'
+        }),
+        new HtmlWebpackPlugin({
+            template: '!!pug-loader!src/landing/views/index.pug',
+            filename: path.join(__dirname, 'index.html')
+        })
+    ]    
+};
+
+const devConfig = {
+    devServer: {
+        historyApiFallback: true
+    }
+};
+
+const prodConfig = {
+    output: {
+        publicPath: 'https://learner1024.github.io/fcc-datavis/dist'
+    }
+}
+
+module.exports = (env) => {    
+    if(env == "production"){
+        return webpackMerge(commonConfig, prodConfig);
+    }
+    else if (env = "development"){
+        return webpackMerge(commonConfig, devConfig);
+    }
 } 
