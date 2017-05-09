9504fd689001f4cc12e9213661380f39d86c1eb3
Sandeep Khandewale <learner1024@gmail.com>
2017-05-09 15:57:45 +0530

    ran webpack --prod

--- dist/apps.bundle.js
+++ dist/apps.bundle.js
@@ -13,15 +13,15 @@ webpackJsonp([0],{
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__components_camper_leaderboard_jsx__ = __webpack_require__("./camper-leaderboard/components/camper-leaderboard.jsx");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react_dist_react_min_js__ = __webpack_require__("../node_modules/react/dist/react.min.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react_dist_react_min_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_react_dist_react_min_js__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react__ = __webpack_require__("../node_modules/react/react.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_react__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_dom__ = __webpack_require__("../node_modules/react-dom/index.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_react_dom__);
 
 
 
 
-__WEBPACK_IMPORTED_MODULE_2_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_1_react_dist_react_min_js___default.a.createElement(__WEBPACK_IMPORTED_MODULE_0__components_camper_leaderboard_jsx__["a" /* default */], null), document.querySelector('#app1Container'));
+__WEBPACK_IMPORTED_MODULE_2_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_0__components_camper_leaderboard_jsx__["a" /* default */], null), document.querySelector('#app1Container'));
 
 /***/ }),
 
@@ -29,8 +29,8 @@ __WEBPACK_IMPORTED_MODULE_2_react_dom___default.a.render(__WEBPACK_IMPORTED_MODU
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js__ = __webpack_require__("../node_modules/react/dist/react.min.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__("../node_modules/react/react.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -80,7 +80,7 @@ var CamperLeaderboard = function (_Component) {
     _createClass(CamperLeaderboard, [{
         key: 'render',
         value: function render() {
-            return __WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js___default.a.createElement(
+            return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
                 'h1',
                 null,
                 'FreeCodeCamp : Camper Leaderboard'
@@ -89,7 +89,7 @@ var CamperLeaderboard = function (_Component) {
     }]);
 
     return CamperLeaderboard;
-}(__WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js__["Component"]);
+}(__WEBPACK_IMPORTED_MODULE_0_react__["Component"]);
 
 /* harmony default export */ __webpack_exports__["a"] = (CamperLeaderboard);
 
@@ -106,9 +106,20 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__styles_style1_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__styles_style1_scss__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_bootstrap_dist_js_bootstrap_min_js__ = __webpack_require__("../node_modules/bootstrap/dist/js/bootstrap.min.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_bootstrap_dist_js_bootstrap_min_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_bootstrap_dist_js_bootstrap_min_js__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_whatwg_fetch__ = __webpack_require__("../node_modules/whatwg-fetch/fetch.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_whatwg_fetch___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_whatwg_fetch__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_promise_polyfill__ = __webpack_require__("../node_modules/promise-polyfill/promise.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_promise_polyfill___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_promise_polyfill__);
 
 
 
+
+
+
+
+if (!window.Promise) {
+    window.Promise = __WEBPACK_IMPORTED_MODULE_4_promise_polyfill___default.a;
+}
 
 /***/ }),
 
@@ -127,8 +138,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__styles_md_previewer_scss__ = __webpack_require__("./md-previewer/styles/md-previewer.scss");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__styles_md_previewer_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__styles_md_previewer_scss__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__components_md_previewer_jsx__ = __webpack_require__("./md-previewer/components/md-previewer.jsx");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_dist_react_min_js__ = __webpack_require__("../node_modules/react/dist/react.min.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_dist_react_min_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_react_dist_react_min_js__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react__ = __webpack_require__("../node_modules/react/react.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_react__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_react_dom__ = __webpack_require__("../node_modules/react-dom/index.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_react_dom__);
 
@@ -144,10 +155,10 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js__ = __webpack_require__("../node_modules/react/dist/react.min.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_remarkable_dist_remarkable_min_js__ = __webpack_require__("../node_modules/remarkable/dist/remarkable.min.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_remarkable_dist_remarkable_min_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_remarkable_dist_remarkable_min_js__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__("../node_modules/react/react.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_remarkable__ = __webpack_require__("../node_modules/remarkable/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_remarkable___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_remarkable__);
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -167,7 +178,7 @@ var MarkdownPreviewer = function (_Component) {
 
         var _this = _possibleConstructorReturn(this, (MarkdownPreviewer.__proto__ || Object.getPrototypeOf(MarkdownPreviewer)).call(this, props));
 
-        _this.markdownParser = new __WEBPACK_IMPORTED_MODULE_1_remarkable_dist_remarkable_min_js___default.a();
+        _this.markdownParser = new __WEBPACK_IMPORTED_MODULE_1_remarkable___default.a();
         _this.state = {
             markdownContent: '',
             htmlMarkup: {
@@ -191,36 +202,36 @@ var MarkdownPreviewer = function (_Component) {
         value: function render() {
             var _this2 = this;
 
-            return __WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js___default.a.createElement(
+            return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
                 'div',
                 null,
-                __WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js___default.a.createElement(
+                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
                     'div',
                     { className: 'row' },
-                    __WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js___default.a.createElement(
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
                         'div',
                         { className: 'col text-center' },
-                        __WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js___default.a.createElement(
+                        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
                             'h1',
                             null,
                             'FreeCodeCamp - Markdown Previewer'
                         )
                     )
                 ),
-                __WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js___default.a.createElement(
+                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
                     'div',
                     { className: 'row' },
-                    __WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js___default.a.createElement(
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
                         'div',
                         { className: 'col' },
-                        __WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js___default.a.createElement('textarea', { className: 'inpt', onChange: function onChange(e) {
+                        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement('textarea', { className: 'inpt', onChange: function onChange(e) {
                                 return _this2.onMarkdownContentChanged(e.target.value);
                             } })
                     ),
-                    __WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js___default.a.createElement(
+                    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
                         'div',
                         { className: 'col' },
-                        __WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js___default.a.createElement('div', { className: 'outpt', dangerouslySetInnerHTML: this.state.htmlMarkup })
+                        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement('div', { className: 'outpt', dangerouslySetInnerHTML: this.state.htmlMarkup })
                     )
                 )
             );
@@ -228,7 +239,7 @@ var MarkdownPreviewer = function (_Component) {
     }]);
 
     return MarkdownPreviewer;
-}(__WEBPACK_IMPORTED_MODULE_0_react_dist_react_min_js__["Component"]);
+}(__WEBPACK_IMPORTED_MODULE_0_react__["Component"]);
 
 /* unused harmony default export */ var _unused_webpack_default_export = (MarkdownPreviewer);
 
