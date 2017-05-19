624c3c0ddd84f040719c80413cfd545bbf26163d
Sandeep Khandewale <learner1024@gmail.com>
2017-05-19 15:10:31 +0530

    array.fil was used incorrectly. shinoks menace is over

--- src/game-of-life/components/game-of-life.jsx
+++ src/game-of-life/components/game-of-life.jsx
@@ -1,11 +1,59 @@
 import React, {Component} from 'react';
+import GameOfLifeEngine from '../lib/GameOfLifeEngine.js';
 
 class GameOfLife extends Component{
     constructor(props){
         super(props);
+        this.rowsCount = 5;
+        this.colsCount = 5;
+        this.engine = new GameOfLifeEngine({
+            rows : this.rowsCount,
+            cols : this.colsCount
+        });
+        this.state = {
+            grid : this.engine.grid
+        };
+    }
+    cellUpdateHandler(rowIndex, colIndex){
+        this.engine.update(rowIndex, colIndex);
+        this.setState({
+            grid: this.engine.grid 
+        });
     }
     render(){
-        return <h1>GOL</h1>
+        return (
+            <div>
+                <table className="table">
+                    <thead>
+                    </thead>
+                    <tbody>
+                        {this.state.grid.map((row, rowIndex) => {
+
+                            return (
+                                <tr key={rowIndex}>
+                                    {
+                                        row.map((col, colIndex) => {
+                                            return(
+                                                <td key={colIndex}>
+                                                    <span onClick={this.cellUpdateHandler.bind(this, rowIndex, colIndex)}>{col}</span>
+                                                </td>
+                                            )
+                                        })
+                                    }
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+                <button onClick={() => {
+                    this.engine.nextGen();
+                    this.setState({
+                        grid: this.engine.grid 
+                    });
+                }} >Next Gen</button>
+            </div>
+
+        )
     }
 }
 
--- src/game-of-life/lib/GameOfLifeEngine.js
+++ src/game-of-life/lib/GameOfLifeEngine.js
@@ -2,20 +2,47 @@ class GameOfLifeEngine{
     constructor(opts){
         this.rowsCount = opts.rows || 10;
         this.colsCount = opts.cols || 10;
-        this.grid = [];
-        this.setEmptyGrid();
-
+        this.grid = this.getEmptyGrid();
     }
-    setEmptyGrid(){
-        this.grid = new Array(this.rowsCount);
-        
-        let row = new Array(this.colsCount)
-        row.fill(0);
-        
-        this.grid.fill(row);
+    getEmptyGrid(){
+        let emptyGrid = [];
+        for(var rowIndex = 0; rowIndex < this.rowsCount; rowIndex ++){
+            let row = [];
+            for(var colIndex = 0; colIndex < this.colsCount; colIndex++){
+                row.push(0);
+            }
+            emptyGrid.push(row);
+        }
+        return emptyGrid;
     }
     nextGen(){
+        let nextGenGrid = this.getEmptyGrid();
 
+        //get alive cells
+        this.grid.forEach((row, rowIndex) => {
+            row.forEach((col, colIndex) => {
+                nextGenGrid[rowIndex][colIndex] = col;
+                let adjacentCells = this.getAdjacentCells(rowIndex, colIndex)
+                let aliveAdjacentCells = adjacentCells.filter((cell) => {
+                    return this.grid[cell[0]][cell[1]] == 1;
+                })
+                if(col == 0){
+                    if(aliveAdjacentCells.length == 3){
+                        nextGenGrid[rowIndex][colIndex] = 1;
+                    }
+                }
+                else if (col == 1){
+                    if(aliveAdjacentCells.length < 2 || aliveAdjacentCells.length > 3){
+                        nextGenGrid[rowIndex][colIndex] = 0;
+                    }
+                }
+                else{
+                    console.log(`Err: ${row}:${col} neither zero nor one!`)
+                }
+            })
+        });
+
+        this.grid = nextGenGrid;
     }
     getAdjacentCells(rowIndex, colIndex){
         let adjacentCellIndices = [];
@@ -51,19 +78,18 @@ class GameOfLifeEngine{
 
         return adjacentCellIndices;
     }
-    start(){
-        this.timer = setInterval(() => {
-            this.nextGen();
-        }, 1000);
-        //keep running logic, until pause or stop
-    }
-    pause(){
-        clearInterval(this.timer);
-    }
+    // start(){
+    //     //this.timer = setInterval(() => {
+    //         this.nextGen();
+    //     //}, 1000);
+    //     //keep running logic, until pause or stop
+    // }
+    // pause(){
+    //     //clearInterval(this.timer);
+    // }
 
     update(rowIndex, colIndex){
-        //update state
-        this.grid[rowIndex][colIndex] = this.grid[rowIndex][colIndex]  == 0 ? 1 : 0;
+        this.grid[rowIndex][colIndex] = this.grid[rowIndex][colIndex] == 1 ? 0 : 1;
     }
 
 }
