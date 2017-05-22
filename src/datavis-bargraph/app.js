4436287e59c78504417cc2c6a2b6725d660ab79c
Sandeep Khandewale <learner1024@gmail.com>
2017-05-22 17:49:04 +0530

    a stupid  svg only version of bar graph

--- src/datavis-bargraph/app.js
+++ src/datavis-bargraph/app.js
@@ -2,23 +2,44 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import * as d3 from 'd3';
 
+import SvgHorizLine from './components/svg-horiz-line.jsx';
+
 class BarGraph extends Component{
+    retrieveDataForBarGraph(){
+        fetch('/data/us-gdp.json')
+            .then((response) => {
+                return response.json();
+            })
+            .then((jsonData) => {
+                this.setState({
+                    data: jsonData.data
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
     constructor(props){
         super(props);
+        //fetch data
+        this.state = {data: []};
+        this.retrieveDataForBarGraph();
     }
     componentDidMount(){
         const square = d3.selectAll('rect');
         square.style('fill', 'orange');
     }
     render(){
+        let distBetweenLines = 5;
+
         return (
             <svg width={this.props.width} height={this.props.height}>
-                <rect x="20" y="20" width="20px" height="20" rx="5" ry="5" />
-                <rect x="60" y="20" width="20px" height="20" rx="5" ry="5" />
-                <rect x="100" y="20" width="20px" height="20" rx="5" ry="5"/>
+                {this.state.data.map((d, i) => {
+                    return (<SvgHorizLine key={i} x1='0' y1={i * distBetweenLines} x2={d[1]} y2={i * distBetweenLines} />)
+                })}
             </svg>
         );
     }
 }
 
-ReactDOM.render(<BarGraph width='300px' height='150px' />, document.querySelector('#app5Container'));
+ReactDOM.render(<BarGraph width='1000' height='1000' />, document.querySelector('#app5Container'));
